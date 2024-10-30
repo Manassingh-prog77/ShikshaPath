6149f93f184d7403b3d7e3a093900bac68f1b77b
Manassingh-prog77 <143158320+Manassingh-prog77@users.noreply.github.com>
2024-10-30 19:30:02 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://lgtv29359:FIvj0cAsMPDO1wKq@base1.7sjr5.mongodb.net/?retryWrites=true&w=majority&appName=Base1"
+const mongoURI = "your mongoose Url"
 
 const connectToMongo = async() =>{
    try{
